2460d4c472ec8655e2a8e036b800489418489902
Vince Moscar <vsm29@cornell.edu>
2024-12-06 22:21:55 -0500

    added playlist file and copied AddSong page to the Playlist tab to provide basis

---
+++ Shared/Playlist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_manager_starter.Shared
+{
+    public sealed class Playlist
+{
+        public string Name { get; set; }
+        public List<Song> Songs { get; set; }
+}
+}
