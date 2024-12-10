d8b5d6274c01b6b6186815c48dbc3004c8391634
Vince Moscar <vsm29@cornell.edu>
2024-12-10 01:23:53 -0500

    slightly modified playlist tab

---
+++ music-manager-start.Data/Models/Playlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using music_manager_starter.Data.Models;
+
+namespace music_manager_starter.Data.Models
+{
+    public sealed class Playlist
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Song> Songs { get; set; }
+    }
+}
